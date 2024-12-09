f70a07514ed84d5e11e77e9640ffb416e05cdc31
= <accounts1@protonmail.com>
2024-12-09 11:16:07 -0800

    day 3 part one done

--- day_02/day_02_2_02.c
+++ day_02/day_02_2_02.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
 void bsort(int *array, int size) {
 	int swap = 1;
@@ -13,6 +12,7 @@ void bsort(int *array, int size) {
 				array[i] = array[i + 1];
 				array[i + 1] = temp;
 				swap = 1;
+
 			}
 		}
 	}
@@ -23,11 +23,11 @@ void display_array(int *array, int size) {
 	for (int i = 0; i < size; i++) {
 		if (i == size - 1) {
 			printf("%d]\n", array[i]);
-		} else {
+		}
+		else {
 			printf("%d, ", array[i]);
 		}
 	}
-
 }
 
 int main() {
@@ -38,56 +38,51 @@ int main() {
 		exit(1);
 	}
 
+	char buffer[128];
 
 	int *numbers = NULL;
 	int safe_reports = 0;
 
-
-	char buffer[100];
+	// Place line from file into buffer
 	while (fgets(buffer, sizeof(buffer), file) != NULL) {
+
+		// Take the line from the buffer and place the numbers from it into numbers array
+		char *token = strtok(buffer, " ");
 		int count = 0;
 
-		bool increase = true;
-		bool decrease = true;
-		bool is_safe = true;
+		int is_increasing = 1;
+		int is_decreasing = 1;
+		int is_safe = 1;
 
-		char *token = strtok(buffer, " ");
+
 		while (token != NULL) {
 			numbers = realloc(numbers, (count + 1) * sizeof(int));
-			if (numbers == NULL) {
-				printf("Unable to allocate memory.\n");
-				exit(1);
-			}
-
 			numbers[count] = atoi(token);
-			count++;
 			token = strtok(NULL, " ");
+			count++;
 		}
-
 
 		for (int i = 0; i < count - 1; i++) {
-
-			// Get an absolute value for the difference.
 			int diff = abs(numbers[i] - numbers[i + 1]);
 			if (diff < 1 || diff > 3) {
-				is_safe = false;
+				is_safe = 0;
 				break;
 			}
 
-			// Check if numbers are increasing or decreasing.
+			// Check if numbers are increasing or decreasing
 			if (numbers[i] > numbers[i + 1]) {
-				increase = false;
-			} 
+				is_increasing = 0;
+			}
 			else if (numbers[i] < numbers[i + 1]) {
-				decrease = false;
+				is_decreasing = 0;
 			}
 			else {
-				is_safe = false;
+				is_safe = 0;
 				break;
 			}
 		}
 
-		if (is_safe && (increase || decrease)) {
+		if (is_safe && (is_increasing || is_decreasing)) {
 			safe_reports++;
 		}
 
@@ -95,5 +90,8 @@ int main() {
 		numbers = NULL;
 	}
 
-	printf("Safe reports: %d\n", safe_reports);
+	fclose(file);
+	printf("%d\n", safe_reports);
+
+	return 0;
 }
--- day_02/day_02_2_03.c
+++ day_02/day_02_2_03.c
@@ -3,31 +3,16 @@
 #include <string.h>
 #include <stdbool.h>
 
-void bsort(int *array, int size) {
-	int swap = 1;
-	while (swap) {
-		swap = 0;
-		for (int i = 0; i < size - 1; i++) {
-			int temp = array[i];
-			if (array[i] > array[i + 1]) {
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
-				swap = 1;
-			}
-		}
-	}
-}
-
 void display_array(int *array, int size) {
 	printf("[");
 	for (int i = 0; i < size; i++) {
 		if (i == size - 1) {
 			printf("%d]\n", array[i]);
-		} else {
+		} 
+		else {
 			printf("%d, ", array[i]);
 		}
 	}
-
 }
 
 int main() {
@@ -38,48 +23,41 @@ int main() {
 		exit(1);
 	}
 
+	char buffer[128];
 
 	int *numbers = NULL;
 	int safe_reports = 0;
 
-
-	char buffer[100];
 	while (fgets(buffer, sizeof(buffer), file) != NULL) {
-		int count = 0;
-
-		bool increase = true;
-		bool decrease = true;
-		bool is_safe = true;
 
 		char *token = strtok(buffer, " ");
+		int count = 0;
+
+		int is_decreasing = true;
+		int is_increasing = true;
+		int is_safe = true;
+
 		while (token != NULL) {
 			numbers = realloc(numbers, (count + 1) * sizeof(int));
-			if (numbers == NULL) {
-				printf("Unable to allocate memory.\n");
-				exit(1);
-			}
-
 			numbers[count] = atoi(token);
-			count++;
 			token = strtok(NULL, " ");
+			count++;
 		}
-
 
 		for (int i = 0; i < count - 1; i++) {
 
-			// Get an absolute value for the difference.
 			int diff = abs(numbers[i] - numbers[i + 1]);
 			if (diff < 1 || diff > 3) {
 				is_safe = false;
 				break;
 			}
 
-			// Check if numbers are increasing or decreasing.
+			// Check if numbers are increasing or decreasing
 			if (numbers[i] > numbers[i + 1]) {
-				increase = false;
-			} 
+				is_increasing = false;
+			}
 			else if (numbers[i] < numbers[i + 1]) {
-				decrease = false;
+				is_decreasing = false;
 			}
 			else {
 				is_safe = false;
@@ -87,7 +65,7 @@ int main() {
 			}
 		}
 
-		if (is_safe && (increase || decrease)) {
+		if (is_safe && (is_decreasing || is_increasing)) {
 			safe_reports++;
 		}
 
@@ -95,5 +73,7 @@ int main() {
 		numbers = NULL;
 	}
 
-	printf("Safe reports: %d\n", safe_reports);
+	printf("%d\n", safe_reports);
+
+	return 0;
 }
