1c01f229079a38c643bffc76b5ced1a6d96616b8
= <accounts1@protonmail.com>
2024-12-09 16:41:27 -0800

    Reimplemented day_1 part 1

--- day_01/day_01_1.c
+++ day_01/day_01_1.c
@@ -1,75 +1,53 @@
+// Day 1 - Part 1
 #include <stdio.h>
 #include <stdlib.h>
 
-int count_lines(char *filename) {
-	int count = 0;
-	char ch;
+int compare(const void *a, const void *b) {
+	return (*(int*)a - *(int*)b);
+}
 
-	FILE *file = fopen(filename, "r");
+int main() {
+
+	FILE *file = fopen("input.txt", "r");
 	if (file == NULL) {
 		printf("Unable to open file.\n");
 		exit(1);
 	}
 
+	char buffer[128];
+	char ch;
+
+	int total = 0;
+	int num1 = 0;
+	int num2 = 0;
+	int lines = 0;
+	int counter = 0;
+
 	while ((ch = fgetc(file)) != EOF) {
 		if (ch == '\n') {
-			count++;
+			lines++;
 		}
 	}
 
-	fclose(file);
+	int left[lines];
+	int right[lines];
 
-	return count;
-}
-
-void bsort(int *array, int size) {
-
-	int swap = 1;
-
-	while (swap) {
-		swap = 0;
-
-		for (int i = 0; i < size - 1; i++) {
-			if (array[i] > array[i + 1]) {
-				int temp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
-				swap = 1;
-			}
-		}
-	}
-}
-
-int main() {
-
-	int lines = count_lines("input.txt");
-	int list_one[lines];
-	int list_two[lines];
-	int total_distance = 0;
-
-	FILE *file = fopen("input.txt", "r");
-	if (file == NULL) {
-		fprintf(stderr, "File not found.\n");
-		exit(1);
+	rewind(file);
+	while (fgets(buffer, 128, file) != NULL) {
+		sscanf(buffer, "%d %d", &num1, &num2);
+		left[counter] = num1;
+		right[counter] = num2;
+		counter++;
 	}
 
-	int count = 0;
-	while (fscanf(file, "%d %d", &list_one[count], &list_two[count]) == 2) {
-		count++;
-	}
-
-	bsort(list_one, lines);
-	bsort(list_two, lines);
+	qsort(left, lines, sizeof(int), compare);
+	qsort(right, lines, sizeof(int), compare);
 
 	for (int i = 0; i < lines; i++) {
-		if (list_one[i] > list_two[i]) {
-			total_distance += list_one[i] - list_two[i];
-		} else {
-			total_distance += list_two[i] - list_one[i];
-		}
+		total += abs(left[i] - right[i]);
 	}
 
-	printf("%d\n", total_distance);
+	printf("%d\n", total);
 
 	return 0;
 }
